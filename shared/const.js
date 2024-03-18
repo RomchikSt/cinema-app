d3aa5c393e5b086a3a4b5f1659045865753b932e
Romchik Stakhiv <Roma.Stakhiv@gmail.com>
2024-03-18 20:37:00 +0200

    feature/top10week

--- shared/const.js
+++ shared/const.js
@@ -1,2 +1,28 @@
 export const TMDB_TOKEN =
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzlhMjZjMzRhMTY5ZWE2NTVjNDdhNmQzZWZhYWMwYSIsInN1YiI6IjY1MWFmNjI3NzQ1MDdkMDBjNTc4NTJlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FJbUVai1ezTwoAIFgEF3IM3_jSz_bh_7mr8y1LQ8d8Q";
+
+export const LANGUAGES = [
+  { language: "English", code: "en-US" },
+  { language: "French", code: "fr-FR" },
+  { language: "German", code: "de-DE" },
+  { language: "Italian", code: "it-IT" },
+  { language: "Spanish", code: "es-ES" },
+  { language: "Russian", code: "ru-RU" },
+  { language: "Japanese", code: "ja-JP" },
+  { language: "Korean", code: "ko-KR" },
+  { language: "Chinese", code: "zh-CN" },
+  { language: "Hindi", code: "hi-IN" },
+  { language: "Portuguese", code: "pt-PT" },
+  { language: "Danish", code: "da-DK" },
+  { language: "Finnish", code: "fi-FI" },
+  { language: "Dutch", code: "nl-NL" },
+  { language: "Swedish", code: "sv-SE" },
+  { language: "Norwegian", code: "nb-NO" },
+  { language: "Polish", code: "pl-PL" },
+  { language: "Turkish", code: "tr-TR" },
+  { language: "Czech", code: "cs-CZ" },
+  { language: "Hungarian", code: "hu-HU" },
+  { language: "Greek", code: "el-GR" },
+  { language: "Arabic", code: "ar-AE" },
+  { language: "Ukrainian", code: "uk-UA" },
+];
